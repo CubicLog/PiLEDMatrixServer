7b887d7c4c097ddf9e9a9df3d48a167937493fda
BuildTools <unconfigured@null.spigotmc.org>
2024-02-12 16:04:03 -0700

    resolution fix

--- ledInterface.py
+++ ledInterface.py
@@ -13,7 +13,7 @@ image = Image.open("EmperixLOGO64bit.png")
 # Configuration for the matrix
 options = RGBMatrixOptions()
 options.rows = 64
-options.columns = 64
+options.cols = 64
 options.chain_length = 1
 options.parallel = 1
 options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
