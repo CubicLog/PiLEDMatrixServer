34cd73ee1094d4ac2d1b66b081c2ea71636aa0a3
BuildTools <unconfigured@null.spigotmc.org>
2024-02-12 16:29:18 -0700

    update

--- ledInterface.py
+++ ledInterface.py
@@ -17,7 +17,7 @@ options.cols = 64
 options.chain_length = 1
 options.parallel = 1
 options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
-options.pwm_lsb_nanoseconds = 50
+options.pwm_lsb_nanoseconds = 300
 
 matrix = RGBMatrix(options = options)
 
